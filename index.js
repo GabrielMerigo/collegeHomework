6c504ebfdce4fb12a1d82cbc275e5540e01ba744
gabriel.merigo <gabriel.merigo.dev@gmail.com>
2022-06-11 16:48:06 -0300

    first step finished

--- index.js
+++ index.js
@@ -2,31 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
-const Person =  require('./models/Person');
+const personRoutes = require('./routes/personRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.post('/person', async (req, res) => {
-
-  const { name, salary, approved } = req.body;
-  console.log()
-  
-  if(!name || !salary || !approved){
-    res.status(422).json({ error: 'Os campos name, salary e approved são obrigatórios.' })
-  }
-
-  const person = { name, salary, approved };
-
-  try{
-    await Person.create(person);
-    res.status(201).json({ message: 'Pessoa inserida com sucesso' });
-  }catch(err){
-    res.status(500).json({ err })
-  }
-
-})
+app.use('/person', personRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'oi Express!'});
--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -5,7 +5,7 @@ router.post('/', async (req, res) => {
   const { name, salary, approved } = req.body;
   
   if(!name || !salary || !approved){
-    res.status(422).json({ error: 'Os campos name, salary e approved são obrigatórios.' });
+    res.status(422).json({ error: 'The fields name, salary and approved are required.' });
     return
   }
 
@@ -13,7 +13,7 @@ router.post('/', async (req, res) => {
 
   try{
     await Person.create(person);
-    res.status(201).json({ message: 'Person was created!' });
+    res.status(201).json({ message: 'Person has been created!' });
   }catch(err){
     res.status(500).json({ err })
   }
@@ -32,15 +32,44 @@ router.get('/:id', async (req, res) => {
   const id = req.params.id;
   try{
     const person = await Person.findOne({ _id: id });
+
+    if(!person){
+      return res.status(422).json({ message: 'Person not found' });
+    }
+
     res.status(200).json(person);
   }catch(err){
     res.status(500).json({ messagem: err.message })
   }
 })
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', async (req, res) => {
+  const id = req.params.id;
+  const { name, salary, approved } = req.body;
+  const person = { name, salary, approved };
+  const doesPersonExist = await Person.findOne({ _id: id }) === null;
+
+  if (doesPersonExist){ 
+    return res.status(422).json({ message: 'Person not found' });
+  }
+
+  try{
+    await Person.updateOne({ _id: id }, person);
+    res.status(200).json({ messagem: 'Person has been updated with success' });
+  }catch(err){
+    res.status(500).json({ messagem: err.message })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
   const id = req.params.id;
   
+  try{
+    await Person.deleteOne({ _id: id});
+    res.status(200).json({ messagem: 'Person has been deleted with success' });
+  }catch (err){
+    res.status(500).json({ messagem: err.message })
+  }
 })
 
 module.exports = router;
