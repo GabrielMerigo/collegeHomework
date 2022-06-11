c9ae456a45dce540663d6b15a140807b8337a053
gabriel.merigo <gabriel.merigo.dev@gmail.com>
2022-06-11 12:56:49 -0300

    creating post API

--- index.js
+++ index.js
@@ -3,8 +3,30 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const Person =  require('./models/Person');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.post('/person', async (req, res) => {
+
+  const { name, salary, approved } = req.body;
+  console.log()
+  
+  if(!name || !salary || !approved){
+    res.status(422).json({ error: 'Os campos name, salary e approved são obrigatórios.' })
+  }
+
+  const person = { name, salary, approved };
+
+  try{
+    await Person.create(person);
+    res.status(201).json({ message: 'Pessoa inserida com sucesso' });
+  }catch(err){
+    res.status(500).json({ err })
+  }
+
+})
 
 app.get('/', (req, res) => {
   res.json({ message: 'oi Express!'});
