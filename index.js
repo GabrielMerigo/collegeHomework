d5713d5fd474e862af1b1519dc8cd7abaabe62db
gabriel.merigo <gabriel.merigo.dev@gmail.com>
2022-06-15 01:06:00 -0300

    ajustes de variaveis ambiente

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = require('./src/server');
 const port = process.env.PORT || 3000;
 
-mongoose.connect(`mongodb+srv://gabriel:flashzika2003@apicluster.knjgws6.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@apicluster.knjgws6.mongodb.net/?retryWrites=true&w=majority`)
   .then(res => {
     app.listen(port);
   })
