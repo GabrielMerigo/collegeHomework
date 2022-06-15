fddbbf959c4484dc9fea8f366697c8bfa6645e0c
gabriel.merigo <gabriel.merigo.dev@gmail.com>
2022-06-14 23:23:48 -0300

    teste

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./src/server');
+const port = process.env.PORT || 3000;
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@apicluster.knjgws6.mongodb.net/?retryWrites=true&w=majority`)
   .then(res => {
-    app.listen(3000);
+    app.listen(port);
   })
   .catch(err => {
     console.log(err)
